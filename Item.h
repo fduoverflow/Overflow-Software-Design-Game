232f884b0e2b6b533c93837d1c67072770163ec6
Shakir Tempelman <shakir33@hotmail.co.uk>
2024-10-24 06:16:26 -0400

    OVERFLOW-28 Set up Item.h file.

--- Item.h
+++ Item.h
@@ -3,10 +3,45 @@
 
 using namespace std;
 
+/*
+* Item class represents items that the Player can use.
+* Each item will have a name, description, and type.
+* Healing items will restore the player's HP
+* Key items can be used in certain situations such as a quest or puzzle.
+* Equipment can be put on by the player to alter their stats.
+* Weapons will affect how the player can attack.
+*/
 class Item
 {
-private:
-	string name;
-	string description;
+	public:
+		//Item types
+		enum class Type
+		{
+			Healing,
+			Key,
+			Equipment,
+			Weapon,
+		};
+
+		//Constructors
+		Item();
+		Item(string n, string desc, Type t, int val);
+
+		//Getters and setters
+		string GetName();
+		string GetDescription();
+		Type GetType();
+		int GetValue();
+		void setName(string n);
+		void setDescription(string desc);
+		void setType(Type t);
+		void setValue(int val);
+
+	private:
+		string name;
+		string description;
+		Type myType;
+		int value;				//This value can be used to represent amount of HP healed, damage a weapon does, armor provided, etc... depending on the item Type.
+	
 };
 
