a8006fb140c184d66f3656d7ae1b4228aea18fa5
Xavier-Orrala <xavierorrala12404@gmail.com>
2024-10-24 23:10:41 -0400

    OVERFLOW-62 Created Quest Constructor holding an Item object for item fetch type quests Commented out the enemyGoal and Quest Constructor with an Enemy object as a parameter as it is currently out of scope.

--- Quest.cpp
+++ Quest.cpp
@@ -1,1 +1,13 @@
 #include "Quest.h"
+Quest::Quest()
+{
+	name = "";
+	prompt = "";
+	itemGoal = Item();
+}
+Quest::Quest(string n, string p, Item i)
+{
+	name = name;
+	prompt = prompt;
+	itemGoal = i;
+}
--- Quest.h
+++ Quest.h
@@ -5,17 +5,18 @@
 
 using namespace std;
 
+// enemyGoal and Constructor with enemy are commented out for now as first quest is an item fetch type quest
 class Quest
 {
 private:
 	string name;
 	string prompt;
 	Item itemGoal;			// The criteria to complete the quest if the criteria is to retrieve an item
-	Enemy enemyGoal;			// The criteria to complete the quest if the criteria is to kill an enemy
+	//Enemy enemyGoal;			// The criteria to complete the quest if the criteria is to kill an enemy
 public:
 	Quest();						// Default constructor
-	Quest(string, string, Item);	// Constructor for an item quest
-	Quest(string, string, Enemy);	// Constructor for an enemy quest
+	Quest(string, string, Item);	// Constructor for an item quest - name, prompt, Item
+	//Quest(string, string, Enemy);	// Constructor for an enemy quest
 };
 
 
