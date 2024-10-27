fae55b2107c588d58c2a369f9a23cc9802d9b275
Joseph Cangiano <joecangiano@outlook.com>
2024-10-26 23:21:02 -0400

    OVERFLOW-43 Edited NPC.h and NPC.cpp files. Created NPC class.

--- NPC.cpp
+++ NPC.cpp
@@ -1,9 +1,14 @@
-#include "NPC.h"
+#include "NPC.h" 
+#include <iostream> 
 
-NPC::NPC() {
+// Constructor definition
+// Initializes the NPC with a specified name and dialogue
+NPC::NPC(string name, string dialogue)
+    : name(name), dialogue(dialogue) {}
 
-}
-
-NPC::NPC(string n, string d) {
-
+// talk() method definition
+// This method displays the NPC's name and dialogue when called
+void NPC::talk() {
+    // Prints a message to the console, showing the NPC's name and their dialogue
+    cout << name << " says: " << dialogue << endl;
 }
--- NPC.h
+++ NPC.h
@@ -1,16 +1,25 @@
-#pragma once
-#include "Entity.h"
-#include <string>
+#ifndef NPC_H 
+#define NPC_H
 
+#include <string> 
 using namespace std;
 
-class NPC:Entity
-{
-private:
-	string name;			// Name of the NPC
-	string dialogue;		// What the NPC will say when interacted with
+class NPC {
 public:
-	NPC();					// Default constructor
-	NPC(string, string);	// Constructor to include name, dialogue
+    // Constructor for NPC
+    // Takes a name (e.g., "Scrummius the Owl") and dialogue string (e.g., "Hellooo! My name is Scrummius the Owl, and I am quite pleased to meet yooou!") as parameters
+    NPC(string name,  string dialogue);
+
+    // Method to initiate a conversation with the NPC
+    // Displays the NPC's dialogue to the player
+    void talk();
+
+private:
+    // Name of the NPC, used to identify the character (e.g., "Scrummius the Owl")
+    string name;
+
+    // Dialogue that the NPC will say to the player
+    string dialogue;
 };
 
+#endif 
